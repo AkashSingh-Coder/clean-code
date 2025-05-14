be6bac2dcf5d34369f0b96fdfc8e93802359aae2
AkashSingh-Coder <akash.singh1@olx.in>
2025-05-14 13:37:46 +0530

    wine and passes test

--- src/test/java/problems/OlxAppBAgedWineTest.java
+++ src/test/java/problems/OlxAppBAgedWineTest.java
@@ -6,38 +6,49 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-public class OlxAppBAgedWineTest {
+public class OlxAppBagedWineTest {
 
-	@Test
-	public void testUpdateQualityAgedWine1() {
-		Item item = new Item("Aged Wine", 4, 3);
+	OlxApp createOlxAppWithItem(String wine,int sellin,int quality){
+		Item item=new Item(wine,sellin,quality);
 		Item[] items = new Item[] { item };
 		OlxApp app = new OlxApp(items);
-		app.updateQuality();
-		assertEquals("Aged Wine", app.items[0].name);
-		assertEquals(3, app.items[0].sellIn);
-		assertEquals(4, app.items[0].quality);
+		return app;
 	}
 
 	@Test
-	public void testUpdateQualityAgedWine2() {
-		Item item = new Item("Aged Wine", -1, 3);
-		Item[] items = new Item[] { item };
-		OlxApp app = new OlxApp(items);
+	public void testUpdateQualityAgedWine1_SellinDecrease1_QualityIncrease1() {
+		OlxApp app=createOlxAppWithItem("Aged Wine",4,3);
+
 		app.updateQuality();
-		assertEquals("Aged Wine", app.items[0].name);
-		assertEquals(-2, app.items[0].sellIn);
-		assertEquals(5, app.items[0].quality);
+
+		Item item=new Item("Aged Wine", 3, 4);
+		assertEqualByValues(item,app.items[0]);
+	}
+
+	@Test
+	public void testUpdateQualityAgedWine2_SellinDecrease1_QualityIncrease2() {
+		OlxApp app=createOlxAppWithItem("Aged Wine",-1,3);
+
+		app.updateQuality();
+
+		Item item=new Item("Aged Wine", -2, 5);
+		assertEqualByValues(item, app.items[0]);
 	}
 
 	@Test
 	public void testUpdateQualityAgedWine3() {
-		Item item = new Item("Aged Wine", 4, 50);
-		Item[] items = new Item[] { item };
-		OlxApp app = new OlxApp(items);
+		OlxApp app=createOlxAppWithItem("Aged Wine",4,50);
+
 		app.updateQuality();
-		assertEquals("Aged Wine", app.items[0].name);
-		assertEquals(3, app.items[0].sellIn);
-		assertEquals(50, app.items[0].quality);
+
+		Item item=new Item("Aged Wine", 3, 50);
+		assertEqualByValues(item, app.items[0]);
+		
+	}
+
+	void assertEqualByValues(Item item,Item item2){
+		assertEquals(item.name,item2.name);
+		assertEquals(item.sellIn,item2.sellIn);
+		assertEquals(item.quality,item2.quality);
 	}
 }
--- src/test/java/problems/OlxAppCBackstagePassesTest.java
+++ src/test/java/problems/OlxAppCBackstagePassesTest.java
@@ -8,25 +8,31 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class OlxAppCBackstagePassesTest {
 	
-	@Test
-	public void testUpdateQualityBackstagePasses1() {
-		Item item = new Item("ColdPlay concert passes", 15, 3);
+	private static final String NAME = "ColdPlay concert passes";
+
+	OlxApp createOlxAppWithItem(String name,int sellin,int quality){
+		Item item=new Item(name,sellin,quality);
 		Item[] items = new Item[] { item };
 		OlxApp app = new OlxApp(items);
+		return app;
+	}
+	@Test
+	public void testUpdateQualityBackstagePasses1() {
+		OlxApp app = createOlxAppWithItem(NAME,15,3);
+
 		app.updateQuality();
-		assertEquals("ColdPlay concert passes",
-				app.items[0].name);
-		assertEquals(14, app.items[0].sellIn);
-		assertEquals(4, app.items[0].quality);
+		
+		Item item=new Item(NAME, 14, 4);
+		assertEqualByValues(item,app.items[0]);
 	}
 
 	@Test
 	public void testUpdateQualityBackstagePasses2() {
-		Item item = new Item("ColdPlay concert passes", 7, 3);
+		Item item = new Item(NAME, 7, 3);
 		Item[] items = new Item[] { item };
 		OlxApp app = new OlxApp(items);
 		app.updateQuality();
-		assertEquals("ColdPlay concert passes",
+		assertEquals(NAME,
 				app.items[0].name);
 		assertEquals(6, app.items[0].sellIn);
 		assertEquals(5, app.items[0].quality);
@@ -34,14 +40,19 @@ public class OlxAppCBackstagePassesTest {
 
 	@Test
 	public void testUpdateQualityBackstagePasses3() {
-		Item item = new Item("ColdPlay concert passes", 4, 3);
+		Item item = new Item(NAME, 4, 3);
 		Item[] items = new Item[] { item };
 		OlxApp app = new OlxApp(items);
 		app.updateQuality();
-		assertEquals("ColdPlay concert passes",
+		assertEquals(NAME,
 				app.items[0].name);
 		assertEquals(3, app.items[0].sellIn);
 		assertEquals(6, app.items[0].quality);
 	}
 
+	void assertEqualByValues(Item item,Item item2){
+		assertEquals(item.name,item2.name);
+		assertEquals(item.sellIn,item2.sellIn);
+		assertEquals(item.quality,item2.quality);
+	}
 }
